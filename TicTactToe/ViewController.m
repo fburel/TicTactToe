6246d4dc0c0435be659e8c012068546d3f3a4b82
Florian BUREL <florian.burel@mac.com>
2014-11-07 17:25:15 +0100

    geg

--- TicTactToe/ViewController.m
+++ TicTactToe/ViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "TicTacToeGameViewModel.h"
+#import "FBTicTacToeGame.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *boardView;
 
+@property (strong) TicTacToeGameViewModel * viewModel;
 @end
 
 @implementation ViewController
@@ -18,8 +22,87 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    self.viewModel = [[TicTacToeGameViewModel alloc]init];
 }
 
+- (void)viewDidLayoutSubviews
+{
+    int rowSize = self.boardView.frame.size.height / 3;
+    int columSize = self.boardView.frame.size.width / 3;
+    
+    for(int row = 0 ; row < 3 ; row++)
+    {
+        for (int column = 0; column < 3; column ++) {
+            
+            // Add a imageView to the board
+            CGRect frame;
+            frame.size.width = columSize;
+            frame.size.height = rowSize;
+            frame.origin.x = rowSize * row;
+            frame.origin.y = columSize * column;
+            
+            UIImageView * imageView = [[UIImageView alloc]initWithFrame:frame];
+           
+            
+            imageView.tag = 10 * column + row;
+            
+            [self.boardView addSubview:imageView];
+            
+            
+            imageView.userInteractionEnabled = YES;
+            
+            [imageView addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(handleTouch:)]];
+        }
+    }
+
+}
+- (IBAction)NewGame:(id)sender {
+    
+    [self.viewModel startNewGame];
+    
+    [self refreshBoard];
+}
+
+- (void) refreshBoard
+{
+    for (int row = 0; row < 3; row++)
+    {
+        for (int column = 0; column < 3; column++)
+        {
+            id mark = [self.viewModel markAtPositionAtRow:row column:column];
+            
+            UIImageView * imageView = (UIImageView *)[self.boardView viewWithTag:column * 10 + row];
+            
+            if([mark isEqualToString:FBTicTacToeGameBubble])
+            {
+                imageView.backgroundColor = [UIColor redColor];
+            }
+            else if ([mark isEqualToString:FBTicTacToeGameBubble])
+            {
+                imageView.backgroundColor = [UIColor blueColor];
+
+            }
+            else
+            {
+                imageView.backgroundColor = [UIColor whiteColor];
+            }
+        }
+
+    }
+}
+
+- (void) handleTouch:(UITapGestureRecognizer *)sender
+{
+    UIView * imageView = sender.view;
+    int column = imageView.tag / 10;
+    int row = imageView.tag - column;
+    
+    [self.viewModel markPositionAtRow:row column:column];
+    
+    [self refreshBoard];
+    
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
