c827d0672e5cdb74e739c950cbe9364d6ceb8f44
Florian BUREL <florian.burel@mac.com>
2014-11-07 14:47:26 +0100

    Initial Commit

---
+++ TicTactToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTactToe
+//
+//  Created by Florian BUREL on 07/11/2014.
+//  Copyright (c) 2014 Florian Burel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTactToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTactToe
+//
+//  Created by Florian BUREL on 07/11/2014.
+//  Copyright (c) 2014 Florian Burel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
